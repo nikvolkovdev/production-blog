fccaf076a2287d538a6fbcaea192cb727c10be9e
nikvolkovdev <nikvolkovmsk@gmail.com>
2023-04-14 18:14:12 +0700

    Added: hot-fix imports svg

--- src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -3,8 +3,8 @@ import { memo } from 'react';
 import { ArticleView } from 'entities/Article';
 import { AppButton, ButtonVariant } from 'shared/ui/AppButton/AppButton';
 import { AppIcon } from 'shared/ui/AppIcon/AppIcon';
-import PlateIcon from '../../../../shared/assets/icons/plate.svg';
-import FullPlateIcon from '../../../../shared/assets/icons/full-plate.svg';
+import PlateIcon from 'shared/assets/icons/plate.svg';
+import FullPlateIcon from 'shared/assets/icons/full-plate.svg';
 import cls from './ArticleViewSelector.module.scss';
 
 interface ArticleViewSelectorProps {
