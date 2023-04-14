4aa4acf642b70bcd7064ac8cb0515b13d42f04bb
nikvolkovdev <nikvolkovmsk@gmail.com>
2023-04-14 19:08:04 +0700

    Fixed: Api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,13 @@
-import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
