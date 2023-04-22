b426fed1b81d6dc61cd600d0594be9c741068985
nikvolkovdev <nikvolkovmsk@gmail.com>
2023-04-22 15:08:35 +0700

    Added: useModal custom hook

---
+++ src/shared/lib/hooks/useModal.tsx
@@ -0,0 +1,66 @@
+import {
+    MutableRefObject, useCallback, useEffect, useRef, useState,
+} from 'react';
+
+interface UseModalProps {
+    onClose?: () => void;
+    isOpen?: boolean;
+    animationDelay: number;
+}
+
+export function useModal(props: UseModalProps) {
+    const {
+        onClose, isOpen, animationDelay,
+    } = props;
+
+    const timeRef = useRef<ReturnType<typeof setTimeout>>() as MutableRefObject<ReturnType<typeof setTimeout>>;
+    const [isMounted, setIsMounted] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
+
+    // const onContentClick = (e: React.MouseEvent) => {
+    //     e.stopPropagation();
+    // };
+
+    // const onContentClick = (e: React.MouseEvent) => {
+    //     e.stopPropagation();
+    // };
+
+    const close = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true);
+            timeRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, animationDelay);
+        }
+    }, [animationDelay, onClose]);
+
+    const onEscape = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            close();
+        }
+    }, [close]);
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onEscape);
+        }
+
+        return () => {
+            window.removeEventListener('keydown', onEscape);
+            clearTimeout(timeRef.current);
+        };
+    }, [isOpen, onEscape]);
+
+    return {
+        isClosing,
+        isMounted,
+        close,
+    };
+}
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,8 +1,7 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import React, {
-    memo, MutableRefObject, ReactNode, useCallback, useEffect, useRef, useState,
-} from 'react';
+import React, { ReactNode } from 'react';
 import { Overlay } from 'shared/Overlay/Overlay';
+import { useModal } from 'shared/lib/hooks/useModal';
 import { Portal } from '../Portal/Portal';
 import cls from './Modal.module.scss';
 
@@ -15,8 +14,6 @@ interface ModalProps {
 }
 
 export const Modal = (props: ModalProps) => {
-    const [isClosing, setIsClosing] = useState(false);
-
     const {
         className,
         children,
@@ -25,46 +22,7 @@ export const Modal = (props: ModalProps) => {
         lazy,
     } = props;
 
-    const timeRef = useRef<ReturnType<typeof setTimeout>>() as MutableRefObject<ReturnType<typeof setTimeout>>;
-    const [isMounted, setIsMounted] = useState(false);
-
-    // const onContentClick = (e: React.MouseEvent) => {
-    //     e.stopPropagation();
-    // };
-
-    const onCloseModal = useCallback(() => {
-        if (onClose) {
-            setIsClosing(true);
-            timeRef.current = setTimeout(() => {
-                onClose();
-                setIsClosing(false);
-            }, 300);
-        }
-    }, [onClose]);
-
-    const onEscape = useCallback((e: KeyboardEvent) => {
-        if (e.key === 'Escape') {
-            onCloseModal();
-        }
-    }, [onCloseModal]);
-
-    useEffect(() => {
-        if (isOpen) {
-            setIsMounted(true);
-        }
-    }, [isOpen]);
-
-    useEffect(() => {
-        if (isOpen) {
-            window.addEventListener('keydown', onEscape);
-        }
-
-        return () => {
-            window.removeEventListener('keydown', onEscape);
-            clearTimeout(timeRef.current);
-        };
-    }, [isOpen, onEscape]);
-
+    const { close, isClosing, isMounted } = useModal({ animationDelay: 300, onClose, isOpen });
     const mods: Mods = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
@@ -77,7 +35,7 @@ export const Modal = (props: ModalProps) => {
     return (
         <Portal>
             <div className={classNames(cls.Modal, mods, [className])}>
-                <Overlay onClick={onCloseModal} />
+                <Overlay onClick={close} />
                 {/* <div className={cls.content} onClick={onContentClick}> */}
                 <div className={cls.content}>
                     {children}
